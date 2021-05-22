4da56ab0d04fffd1559248c13cf328bd434c1682
Kirill Ki <kirillkalapov@gmail.com>
2021-05-22 10:32:38 +0200

    Final with sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -18,6 +19,29 @@ class ViewController: UIViewController {
     var totalTime = 0
     var secondsPassed = 0
     
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
         
     
@@ -57,6 +81,7 @@ class ViewController: UIViewController {
             timer.invalidate()
             titleLabel.text = "Done"
             print("Sound")
+            playSound()
            }
         
    
